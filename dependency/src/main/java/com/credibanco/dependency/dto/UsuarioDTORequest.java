dfdfb0c642ab0ca8b6fe52a58b4b6790fe8fa7f6
Giyyu Kato <89874341+GiyyuKato@users.noreply.github.com>
2023-06-07 16:04:14 -0500

    Add files via upload

---
+++ dependency/src/main/java/com/credibanco/dependency/dto/UsuarioDTORequest.java
@@ -0,0 +1,10 @@
+package com.credibanco.dependency.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize
+@JsonIgnoreProperties (ignoreUnknown = true)
+public class UsuarioDTORequest extends UsuarioDTOResponse{
+
+}
